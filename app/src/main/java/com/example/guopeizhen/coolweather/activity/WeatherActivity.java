3b1b04dc07d4203501cf25805ad1e1203da6f9f4
Tom <Tom_GPZ@163.com>
2017-08-25 16:49:27 +0800

    Signed-off-by: Tom <Tom_GPZ@163.com>

--- app/src/main/java/com/example/guopeizhen/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/guopeizhen/coolweather/activity/WeatherActivity.java
@@ -1,11 +1,137 @@
 package com.example.guopeizhen.coolweather.activity;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.guopeizhen.coolweather.R;
+import com.example.guopeizhen.coolweather.gson.Forecast;
+import com.example.guopeizhen.coolweather.gson.Weather;
+import com.example.guopeizhen.coolweather.util.HttpUtil;
+import com.example.guopeizhen.coolweather.util.ToastUtil;
+import com.example.guopeizhen.coolweather.util.Utility;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2017/8/25.
  */
 
 public class WeatherActivity extends AppCompatActivity {
+    private ScrollView weatherLayout;
+    private TextView titleCity,titleUpdateTime,degreeText,weatherInfoText,aqiText,pm25Text,
+                    comfortText,carWashText,sportText;
+    private LinearLayout forecastLayout;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        init();
+        String weatherId = getIntent().getStringExtra("weather_id");
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString = preferences.getString("weather",null);
+        if (weatherString!=null) {
+            Weather weather = Utility.handleWeatherJson(weatherString);
+            showWeatherInfo(weather);
+        }else {
+
+
+            requestWeather(weatherId);
+        }
+    }
+
+    private void init(){
+        weatherLayout = (ScrollView)findViewById(R.id.sv_weatherlayout);
+        titleCity = (TextView)findViewById(R.id.tv_title_city);
+        titleUpdateTime = (TextView)findViewById(R.id.tv_title_updatetime);
+        degreeText = (TextView)findViewById(R.id.tv_degreetext);
+        weatherInfoText = (TextView)findViewById(R.id.tv_weatherinfo);
+        aqiText = (TextView)findViewById(R.id.tv_aqitext);
+        pm25Text = (TextView)findViewById(R.id.tv_pm25text);
+        comfortText = (TextView)findViewById(R.id.tv_comforttext);
+        carWashText = (TextView)findViewById(R.id.tv_carwash);
+        sportText = (TextView)findViewById(R.id.tv_sporttext);
+        forecastLayout = (LinearLayout)findViewById(R.id.forecast_layout);
+    }
+
+    private void requestWeather(final String weatherId){
+        String url = "http://guolin.tech/api/weather?cityid="+weatherId+
+                "&key=7d69594b0c7e4b1db52d2254a7d8318f";
+        HttpUtil.sendHttpRequest(url, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ToastUtil.showToast("天气信息加载失败！");
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                Log.i("WeatherActivity",responseText);
+                final Weather weather = Utility.handleWeatherJson(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (weather!=null&&"ok".equals(weather.status)){
+                            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("weather",responseText);
+                            editor.apply();
+                            showWeatherInfo(weather);
+                        }else {
+                            Log.d("WeatherActivity",weather.status);
+                            ToastUtil.showToast("加载天气信息失败！");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void showWeatherInfo(Weather weather){
+        titleCity.setText(weather.basic.cityName);
+        titleUpdateTime.setText(weather.basic.update.updateTime.split(" ")[1]);
+        degreeText.setText(weather.now.temperature+"℃");
+        weatherInfoText.setText(weather.now.more.info);
+        forecastLayout.removeAllViews();
+        for (Forecast forecast : weather.forecastList){
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item,forecastLayout,false);
+            TextView date = view.findViewById(R.id.tv_date);
+            TextView info = view.findViewById(R.id.tv_info);
+            TextView max = view.findViewById(R.id.tv_max);
+            TextView min = view.findViewById(R.id.tv_min);
+            date.setText(forecast.date);
+            info.setText(forecast.more.info);
+            max.setText(forecast.temperature.max);
+            min.setText(forecast.temperature.min);
+            forecastLayout.addView(view);
+        }
+        if (weather.aqi!=null){
+            aqiText.setText(weather.aqi.aqiCity.aqi);
+            pm25Text.setText(weather.aqi.aqiCity.pm25);
+        }
+
+        comfortText.setText("舒适度:" + weather.suggestion.comfort.info);
+        carWashText.setText("洗车指数:" + weather.suggestion.carWash.info);
+        sportText.setText("运动建议:"+ weather.suggestion.sport.info);
+        weatherLayout.setVisibility(View.VISIBLE);
+    }
 }
--- app/src/main/java/com/example/guopeizhen/coolweather/gson/Forecast.java
+++ app/src/main/java/com/example/guopeizhen/coolweather/gson/Forecast.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.SerializedName;
  */
 
 public class Forecast {
-    public String Data;
+    public String date;
 
     @SerializedName("cond")
     public More more;
