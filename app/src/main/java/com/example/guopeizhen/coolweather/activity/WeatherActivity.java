4f7f55cfe7e3adb6949b65ab359641c4b69d01db
Tom <Tom_GPZ@163.com>
2017-08-25 12:07:02 +0800

    新建WeatherActivity类

---
+++ app/src/main/java/com/example/guopeizhen/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,11 @@
+package com.example.guopeizhen.coolweather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/8/25.
+ */
+
+public class WeatherActivity extends AppCompatActivity {
+
+}
